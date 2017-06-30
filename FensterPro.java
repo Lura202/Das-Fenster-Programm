70466f227760b6da806d38e49fb9e1d6dbb1fd25
Lura202 <l.rang@gmx.de>
2017-06-30 19:50:04 +0200

    Added simple buttons
    The buttons disappear after clicking

--- FensterPro.java
+++ FensterPro.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.Color;
-import java.awt.Event;
-class FensterPro {
-  Fenster f;   
+import java.awt.event.*;
+class FensterPro implements ActionListener {
+  Fenster f;  
+  JButton b1; 
+  JButton b2;
   public static void main(String[] args){  
     FensterPro a = new FensterPro();
     a.haupt();
@@ -10,9 +12,29 @@ class FensterPro {
    
   public void haupt() {
     f = new Fenster("Das Fenster Programm");
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setVisible(true);
+    b1 = new JButton("Press Me");
+    b2 = new JButton("Press Me 2");
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
     f.setBounds(300,300,300,300);
+    f.setLayout(null);
+    b1.setBounds(20,20,250,35);
+    b2.setBounds(20,100,250,35);
+    b1.addActionListener(this); 
+    b2.addActionListener(this);
+    f.add(b1);
+    f.add(b2);
+    b1.setVisible(true);
+    b2.setVisible(true);
+    f.setVisible(true);   
+  }
+                                                 
+  public void actionPerformed(ActionEvent e) {
+    if (e.getSource() == b1) {
+      b1.setVisible(false);  
+    } // end of if
+    else{
+      b2.setVisible(false);
+      } 
   }
 }
     
